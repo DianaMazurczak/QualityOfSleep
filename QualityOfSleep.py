99841adfd0e64a6e4e7639d666232d165d0b0059
dmazurczak@student.agh.edu.pl <dmazurczak@student.agh.edu.pl>
2024-11-27 19:40:33 +0100

    feature for lr

--- QualityOfSleep.py
+++ QualityOfSleep.py
@@ -3,8 +3,15 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
+from matplotlib.gridspec import GridSpec
+from sklearn.model_selection import train_test_split
+import random
 
+import statsmodels.api as sm
+from ISLP.models import (ModelSpec as MS,
+ summarize,
+ poly)
 
 data = pd.read_csv("Sleep_health_and_lifestyle_dataset.csv")
-
-print(data)
+# C:\Users\diana\PycharmProjects\QualityOfSleep\.venv\Scripts\activate.bat
+# pip install ISLP
