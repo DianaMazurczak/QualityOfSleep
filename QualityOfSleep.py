85dde3d4e7044de68ca5c271c86468bf78111bf1
dmazurczak@student.agh.edu.pl <dmazurczak@student.agh.edu.pl>
2025-01-26 20:12:05 +0100

    comparison of models

--- QualityOfSleep.py
+++ QualityOfSleep.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from plottable import Table
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 from matplotlib.gridspec import GridSpec
@@ -14,3 +15,18 @@ from ISLP.models import (ModelSpec as MS,
 data = pd.read_csv("Sleep_health_and_lifestyle_dataset.csv")
 # C:\Users\diana\PycharmProjects\QualityOfSleep\.venv\Scripts\activate.bat
 # pip install ISLP
+
+data[['Systolic', 'Diastolic']] = data['Blood Pressure'].str.split('/', expand=True)
+data['Systolic'] = data['Systolic'].astype(int)
+data['Diastolic'] = data['Diastolic'].astype(int)
+data['BMI Category'] = data['BMI Category'].replace('Normal Weight', 'Normal')
+dummies_gender = pd.get_dummies(data['Gender'])
+dummies_Occupation = pd.get_dummies(data['Occupation'])
+dummies_SpleepDisorder = pd.get_dummies(data['Sleep Disorder'])
+numeric_BMICategory = data['BMI Category'].replace(['Normal', 'Overweight', 'Obese'], [0, 1, 2])
+data = pd.concat(
+    [data[['Age', 'Sleep Duration', 'Quality of Sleep', 'Physical Activity Level', 'Stress Level', 'Heart Rate',
+           'Daily Steps', 'Systolic', 'Diastolic']],
+     dummies_gender, dummies_Occupation, dummies_SpleepDisorder, numeric_BMICategory], axis=1)
+
+print(data.columns)
