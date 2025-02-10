46db9ce80afa1fe0889bb10b6cae8c0610dcf086
dmazurczak@student.agh.edu.pl <dmazurczak@student.agh.edu.pl>
2025-02-10 14:22:03 +0100

    comparison of models

--- QualityOfSleep.py
+++ QualityOfSleep.py
@@ -12,8 +12,7 @@ from sklearn import svm
 import random
 
 import statsmodels.api as sm
-from ISLP.models import (ModelSpec as MS,
- summarize)
+from ISLP.models import (ModelSpec as MS, summarize)
 
 data = pd.read_csv("Sleep_health_and_lifestyle_dataset.csv")
 # C:\Users\diana\PycharmProjects\QualityOfSleep\.venv\Scripts\activate.bat
@@ -25,11 +24,11 @@ data['Diastolic'] = data['Diastolic'].astype(int)
 data['BMI Category'] = data['BMI Category'].replace('Normal Weight', 'Normal')
 dummies_gender = pd.get_dummies(data['Gender'])
 dummies_Occupation = pd.get_dummies(data['Occupation'])
-dummies_SpleepDisorder = pd.get_dummies(data['Sleep Disorder'])
+dummies_SleepDisorder = pd.get_dummies(data['Sleep Disorder'])
 numeric_BMICategory = data['BMI Category'].replace(['Normal', 'Overweight', 'Obese'], [0, 1, 2])
 data = pd.concat(
     [data[['Age', 'Sleep Duration', 'Quality of Sleep', 'Physical Activity Level', 'Stress Level', 'Heart Rate',
            'Daily Steps', 'Systolic', 'Diastolic']],
-     dummies_gender, dummies_Occupation, dummies_SpleepDisorder, numeric_BMICategory], axis=1)
+     dummies_gender, dummies_Occupation, dummies_SleepDisorder, numeric_BMICategory], axis=1)
 
 print(data.columns)
